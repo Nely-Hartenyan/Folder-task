17331f7e6af72eb786644d5beb5ba28c7f05d2c4
Nely-Hartenyan <hartenyan.nely1498@gmail.com>
2021-09-23 16:14:09 +0400

    Arrows

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Form from "./Form/Form";
 import Item from "./Items/Item";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import {useHistory} from "react-router-dom";
 
 
 const Container = ({ items }) => {
 
+    const history = useHistory()
     const item = items.filter( item => item.parent === '0')
 
     return (
         <div>
+            <ArrowForwardIcon onClick = {() => history.goForward()}/>
             <Form items = { items }/>
             { item.map(item => {
                 return (
--- src/Components/FolderAndTextItems/FolderItems.js
+++ src/Components/FolderAndTextItems/FolderItems.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import {useHistory, useParams} from "react-router-dom";
 import Form from "../Form/Form";
 import Item from "../Items/Item";
-
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
 const FolderItems = ( { items } ) => {
 
     const { Folderid } = useParams()
+    const history = useHistory()
 
     const folders = items.filter( item => item.parent === Folderid )
 
 return(
         <div>
+            <ArrowBackIcon onClick = {() => history.goBack()}/>
+           <ArrowForwardIcon onClick = {() => history.goForward()}/>
+
             <Form items = { items }/>
             { folders.map( item => {
                 return (
